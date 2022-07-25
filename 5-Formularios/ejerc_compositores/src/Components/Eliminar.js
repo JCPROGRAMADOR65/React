e17fd462444789cba0c3e4a72fea7198095f6dbc
JCPROGRAMADOR65 <jcrino@gmail.com>
2022-07-26 01:33:56 +0200

    5-Formularios_Compositores

---
+++ 5-Formularios/ejerc_compositores/src/Components/Eliminar.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Eliminar(props) {
+  const [erase, setErase] = useState('');
+
+  function borrarCompositor(nombre) {
+    let array = props.init.filter(
+     compositor => {return compositor.nombre !== nombre}
+   ) 
+    props.setInit(array);
+    setErase('');
+  }
+
+  return (
+    <div>
+      <input
+        type='text' onChange={
+       (e) => {setErase(e.target.value)}
+       }
+        value={erase}
+      />
+      <button onClick={
+       () => {borrarCompositor(erase)}
+       } >
+        Borrar
+      </button>
+    </div>
+  );
+}
+export default Eliminar;
---
+++ 5-Formularios/ejerc_compositores/src/Components/Lista.js
@@ -0,0 +1,10 @@
+function Lista(props) {
+  return (
+    <ul>
+      {props.lista.map((compositor, index) => (
+        <li key={index}>{compositor.nombre}</li>
+      ))}
+    </ul>
+  );
+}
+export default Lista;
